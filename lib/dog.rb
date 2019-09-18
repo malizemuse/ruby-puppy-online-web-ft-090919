57abf06cf1e23b58ba5fac8f73b2233836549853
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-18 21:20:50 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
-# Add your code here
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+    self.save
+  end 
+  
+  def save
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.print_all 
+    @@all.collect { |dog|
+      puts dog.name
+    }
+  end 
+  
+  def self.clear_all 
+    @@all.clear 
+  end
+  
+end 
+
+
